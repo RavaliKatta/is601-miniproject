26a9fd6591bff7ab2c13bb27193a46ad42750c00
Ravali <rk694@njit.edu>
2018-10-04 22:04:00 -0700

    Task: Added record class in php file.

--- public/index.php
+++ public/index.php
@@ -19,13 +19,33 @@ class main{
 class csv{
     static public function getRecords($filename){
         $file = fopen($filename, "r");
-        while(!feof($file))
+
+        while(! feof($file))
         {
             $record = fgetcsv($file);
 
+
                 $records[] = $record;
             }
+
         fclose($file);
         return $records;
+    }
+}
+
+class record {
+    public function __construct(Array $fieldNames = null, $values = null )
+    {
+        $record = array_combine($fieldNames, $values);
+        foreach ($record as $property => $value) {
+            $this->createProperty($property, $value);
+        }
+    }
+    public function returnArray() {
+        $array = (array) $this;
+        return $array;
+    }
+    public function createProperty($name = 'Company', $value = 'Infosys') {
+        $this->{$name} = $value;
     }
 }
