9f4569730d41f6cbc56429cbb4379ce54f61d0c0
Ravali <rk694@njit.edu>
2018-10-05 22:18:50 -0700

    Task: Created record class in PHP file.

--- public/index.php
+++ public/index.php
@@ -25,3 +25,19 @@ class csv{
         return $records;
     }
 }
+class record {
+    public function __construct(Array $fieldNames = null, $values = null )
+    {
+        $record = array_combine($fieldNames, $values);
+        foreach ($record as $property => $value) {
+            $this->createProperty($property, $value);
+        }
+    }
+    public function returnArray() {
+        $array = (array) $this;
+        return $array;
+    }
+    public function createProperty($name = 'Company', $value = 'Infosys') {
+        $this->{$name} = $value;
+    }
+} 
