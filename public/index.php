852b922a7bcdbee27c8767a3d74cce1de893fbe0
Ravali <rk694@njit.edu>
2018-10-05 22:29:22 -0700

    Task: Created html class and data in PHP file.

--- public/index.php
+++ public/index.php
@@ -10,7 +10,26 @@ main::start("datatable.csv");
 class main{
     static public function start($filename){
         $records = csv::getRecords($filename);
-        print_r($records);
+        $table = html::generateTable($records);
+    }
+}
+class html {
+    public static function generateTable($records) {
+        $count = 0;
+        foreach ($records as $record) {
+            if($count == 0) {
+                $array = $record->returnArray();
+                $fields = array_keys($array);
+                $values = array_values($array);
+                print_r($fields);
+                print_r($values);
+            } else {
+                $array = $record->returnArray();
+                $values = array_values($array);
+                print_r($values);
+            }
+            $count++;
+        }
     }
 }
 class csv{
